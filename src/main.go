784e9decf6aad5d77671f3427d1bcd4c9fff954e
Jason Chen <ai.ironman.jason@gmail.com>
2018-01-22 23:00:40 +0800

    Init check in for go practise.

---
+++ src/main.go
@@ -0,0 +1,27 @@
+// main.go project main.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var page = "something in html"
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, page)
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/page", pageHandler)
+	err := http.ListenAndServe("localhost:8088", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
